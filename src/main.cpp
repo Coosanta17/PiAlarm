7ba2b98389f1847f565d24fb8325165e5d625d7f
Coosanta <coosantamapy@gmail.com>
2025-04-16 21:29:31 +1000

    Looks like csignal was still being used. Even in a small way!!

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <pigpio.h>
-// #include "../external/pigpio.h" // For development purposes only to remove red text
+#include "../external/pigpio.h" // For development purposes only to remove red text
 #include <unistd.h>
+#include <csignal>
 #include "../include/alarm.h"
 
 #define BUZZER_GPIO 12  // PWM 0
